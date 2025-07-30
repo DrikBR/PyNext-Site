29862a2497bea21fb85b07443f294e5722365e33
Endrik <endriksantos19@gmail.com>
2025-07-30 13:08:06 -0300

    2 primeiras telas e trocar modo

---
+++ trocar_modo.js
@@ -0,0 +1,40 @@
+function trocar() {
+
+    //para funcionar, lembrar de colocar os IDS no html e nas logos!!!
+
+    const botão = document.getElementById('trocar');
+    const site = document.getElementById('site');
+    const icone = document.getElementById('icone');
+    const textobot = document.getElementById('textobot');
+    const logo = document.getElementById('logo');
+    const fundo = document.getElementById('fundo');
+
+    botão.addEventListener('click', () => {
+
+        site.classList.toggle('dark');
+
+        const siteescuro = site.classList.contains('dark');
+
+        if (siteescuro) {
+            icone.src = "./imagens/sol.png";
+            textobot.textContent = "Modo Claro";
+            if (logo) {
+                logo.src = "./imagens/logoescura.png";
+            }
+            if (fundo) {
+                fundo.style.backgroundImage = "url('./imagens/logoescura.png')";
+            }
+            
+        } else {
+            icone.src = "./imagens/lua.png";
+            textobot.textContent = "Modo Escuro";
+            if (logo) {
+                logo.src = "./imagens/logoclara.png";
+            }
+            if (fundo) {
+                fundo.style.backgroundImage = "url('./imagens/logoclara.png')";
+            }
+        }
+    });
+}
+window.addEventListener("DOMContentLoaded", trocar);
